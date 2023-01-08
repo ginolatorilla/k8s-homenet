88dd94f7f15b8ffabfeb330080e96fdf1d47b27a
Gino Latorilla <gino.latorilla@gamesysgroup.com>
2023-01-08 18:49:37 +0800

    Resilient DNS

--- host-0-darwin-aarch64/scripts/install-dns.sh
+++ host-0-darwin-aarch64/scripts/install-dns.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
-systemctl disable systemd-resolved.service 
-systemctl stop systemd-resolved.service
-
 export DEBIAN_FRONTEND=noninteractive
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "If dnsmasq fails to start during installation, ignore it."
+echo "We will have to stop systemd-resolved after (we can't stop if before"
+echo "because we need to resolve Ubuntu servers)."
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
 apt-get update
 apt-get install -y dnsmasq
+
+systemctl disable systemd-resolved
+systemctl stop systemd-resolved
+systemctl enable dnsmasq
+systemctl restart dnsmasq
