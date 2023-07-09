3a499f153c7f152f06553a85d3842f8279ba253f
Gino Latorilla <pblatorilla+github@gmail.com>
2023-07-09 12:36:12 +0800

    Migrate base script as provisioner, fix sticky ips

--- lib/node-dns.rb
+++ lib/node-dns.rb
@@ -4,9 +4,8 @@ require_relative 'node-base.rb'
 def dns(config, vmhost:, name:, macaddr:)
     return if Socket.gethostname != vmhost
 
-    base(config, vmhost)
-
     config.vm.define name do |node|
+        base(config, vmhost, reboot_after_up: false)
         node.vm.hostname = name
 
         node.vm.provider :vmware_desktop do |v|
@@ -16,23 +15,24 @@ def dns(config, vmhost:, name:, macaddr:)
             v.vmx["ethernet0.address"] = macaddr
         end
 
-        node.vm.provision "install", type: "shell", inline: <<-SCRIPT
-            /vagrant/scripts/install-base.sh
+        node.vm.provision "install", type: "shell", inline: <<~SCRIPT
+            export DEBIAN_FRONTEND=noninteractive
+
+            apt-get install -y dnsmasq
 
             echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            echo "If dnsmasq fails to start during installation, ignore it."
-            echo "We will have to stop systemd-resolved after (we can't stop if before"
-            echo "because we need to resolve Ubuntu servers)."
+            echo "If dnsmasq failed during installation with this error message:"
+            echo "    failed to create listening socket for port 53: Address already in use"
+            echo "Ignore it."
             echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-            apt-get install -y dnsmasq
-            
+
             systemctl disable systemd-resolved
             systemctl stop systemd-resolved
             systemctl enable dnsmasq
             systemctl restart dnsmasq
         SCRIPT
 
-        node.vm.provision "reload", after: "install", type: "shell", inline: <<-SCRIPT
+        node.vm.provision "reload", after: "install", type: "shell", run: "never", inline: <<~SCRIPT
             cp /vagrant/etc/dnsmasq.conf /etc/dnsmasq.conf
             systemctl restart dnsmasq
         SCRIPT
