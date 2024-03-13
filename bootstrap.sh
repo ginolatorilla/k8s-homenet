aa236ab8cadd4ccf5c6dff7d56806b9687fe77d1
Gino Latorilla <pblatorilla+github@gmail.com>
2024-03-14 00:28:35 +0800

    Add inventory

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 pushd "$(dirname "${BASH_SOURCE[0]}")/ansible" || exit
-ansible-playbook create_cluster.yaml -K "$@"
+ansible-playbook create_cluster.yaml -K -i inventory.yaml "$@"
 popd || exit
